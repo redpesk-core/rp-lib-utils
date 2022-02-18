ed6fd38de10fe107fd4b8d325f1c07631760ca10
Jose Bollo <jose.bollo@iot.bzh>
2022-02-18 18:02:08 +0100

    update copyrights

--- src/misc/expand-vars.c
+++ src/misc/expand-vars.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2021 IoT.bzh Company
+ * Copyright (C) 2020-2022 IoT.bzh Company
  * Author: José Bollo <jose.bollo@iot.bzh>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
