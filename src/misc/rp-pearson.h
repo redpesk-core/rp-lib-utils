54b5d389182d908924e4ec8f6deaf12790ab74e7
Jose Bollo <jose.bollo@iot.bzh>
2022-08-29 18:26:21 +0200

    rp-pearson: Improve documentation

--- src/misc/rp-pearson.h
+++ src/misc/rp-pearson.h
@@ -31,7 +31,7 @@
  * Returns a 8 bits hash value for the 'text'.
  *
  * Tiny hash function inspired from pearson
- * 
+ *
  * @param text the text to hash
  * @return a 8 bits hash value
  */
@@ -41,7 +41,7 @@ extern uint8_t rp_pearson8(const char *text);
  * Returns a 8 bits hash value for the 'text' of 'length'.
  *
  * Tiny hash function inspired from pearson
- * 
+ *
  * @param text the text to hash
  * @param length length of the text to hash
  * @return a 8 bits hash value
@@ -50,23 +50,125 @@ extern uint8_t rp_pearson8_len(const char *text, size_t length);
 
 /* defines sub length pearson using inlines */
 
-#define _DEF_RP_PEARSON_(N) \
-		inline uint8_t rp_pearson##N(const char *text) {\
-			uint8_t h = rp_pearson8(text);\
-			return ((h ^ (h >> (8 - (N)))) & (255 >> (8 - (N)))); }
+#define _DEF_RP_PEARSON_(N) {\
+		uint8_t h = rp_pearson8(text);\
+		return ((h ^ (h >> (8 - (N)))) & (255 >> (8 - (N)))); }
 
-#define _DEF_RP_PEARSON_LEN_(N) \
-		inline uint8_t rp_pearson##N##_len(const char *text, size_t length) {\
-			uint8_t h = rp_pearson8_len(text, length);\
-			return ((h ^ (h >> (8 - (N)))) & (255 >> (8 - (N)))); }
+#define _DEF_RP_PEARSON_LEN_(N) {\
+		uint8_t h = rp_pearson8_len(text, length);\
+		return ((h ^ (h >> (8 - (N)))) & (255 >> (8 - (N)))); }
 
+/*
+ * Returns a 4 bits hash value for the 'text'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @return a 3 bits hash value
+ */
+inline uint8_t rp_pearson3(const char *text)
 _DEF_RP_PEARSON_(3)
+
+/*
+ * Returns a 3 bits hash value for the 'text' of 'length'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @param length length of the text to hash
+ * @return a 3 bits hash value
+ */
+inline uint8_t rp_pearson3_len(const char *text, size_t length)
 _DEF_RP_PEARSON_LEN_(3)
+
+/*
+ * Returns a 4 bits hash value for the 'text'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @return a 4 bits hash value
+ */
+inline uint8_t rp_pearson4(const char *text)
 _DEF_RP_PEARSON_(4)
+
+/*
+ * Returns a 4 bits hash value for the 'text' of 'length'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @param length length of the text to hash
+ * @return a 4 bits hash value
+ */
+inline uint8_t rp_pearson4_len(const char *text, size_t length)
 _DEF_RP_PEARSON_LEN_(4)
+
+/*
+ * Returns a 5 bits hash value for the 'text'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @return a 5 bits hash value
+ */
+inline uint8_t rp_pearson5(const char *text)
 _DEF_RP_PEARSON_(5)
+
+/*
+ * Returns a 5 bits hash value for the 'text' of 'length'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @param length length of the text to hash
+ * @return a 5 bits hash value
+ */
+inline uint8_t rp_pearson5_len(const char *text, size_t length)
 _DEF_RP_PEARSON_LEN_(5)
+
+/*
+ * Returns a 6 bits hash value for the 'text'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @return a 6 bits hash value
+ */
+inline uint8_t rp_pearson6(const char *text)
 _DEF_RP_PEARSON_(6)
+
+/*
+ * Returns a 6 bits hash value for the 'text' of 'length'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @param length length of the text to hash
+ * @return a 6 bits hash value
+ */
+inline uint8_t rp_pearson6_len(const char *text, size_t length)
 _DEF_RP_PEARSON_LEN_(6)
+
+/*
+ * Returns a 7 bits hash value for the 'text'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @return a 7 bits hash value
+ */
+inline uint8_t rp_pearson7(const char *text)
 _DEF_RP_PEARSON_(7)
+
+/*
+ * Returns a 7 bits hash value for the 'text' of 'length'.
+ *
+ * Tiny hash function inspired from pearson
+ *
+ * @param text the text to hash
+ * @param length length of the text to hash
+ * @return a 7 bits hash value
+ */
+inline uint8_t rp_pearson7_len(const char *text, size_t length)
 _DEF_RP_PEARSON_LEN_(7)
