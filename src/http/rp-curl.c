c59636bf1d8dde01ec764134e767e1324af82d1a
José Bollo <jose.bollo@iot.bzh>
2026-08-18 15:29:06 +0200

    Refactor of rp-curl
    Previous version has not been used until now and was unfortunately
    not usable. That new version is a refactoring more usable.

--- src/http/rp-curl.c
+++ src/http/rp-curl.c
@@ -32,151 +32,89 @@
 
 #include "rp-escape.h"
 
+static const char content_type[] = "Content-Type";
 
-/* internal representation of buffers */
-struct buffer {
-	size_t size;
-	char *data;
-};
+/*
+ * Perform the CURL operation for 'curl' and call given callback
+ */
+int rp_curl_do(
+	CURL *curl,
+	struct curl_slist *headers,
+	size_t (*callback)(char *buffer, size_t size, size_t nitems, void *userdata),
+	void *userdata
+) {
+	CURLcode code;
 
-static const char* curl_concatenate_args(const char * const *args, const char *sep, size_t *length)
-{
-	unsigned i;
-	size_t lq;
-	const char *null;
-	char *result, *front;
-
-	/* ensure args */
-	if (!args) {
-		null = NULL;
-		args = &null;
+	/* Perform the request, res will get the return code */
+	if (headers != NULL) {
+		code = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		if (code != CURLE_OK)
+			return -1;
 	}
-	if (!sep)
-		sep = "";
-
-	/* compute lengths */
-	lq = 0;
-	i = 0;
-	while (args[i]) {
-		lq += strlen(args[i]);
-		i++;
-	}
-
-	/* allocation */
-	result = malloc(1 + lq + (i ? i - 1 : 0) * strlen(sep));
-	if (result) {
-		/* make the resulting args string contenated */
-		i = 0;
-		front = result;
-		while (args[i]) {
-			if (i) {
-				front = stpcpy(front, sep);
-			}
-			front = stpcpy(front, args[i]);
-			i++;
-		}
-		*front = 0;
-		if (length)
-			*length = (size_t)(front - result);
-	}
-	return result;
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, userdata);
+	code = curl_easy_perform(curl);
+	return -(code != CURLE_OK);
 }
+
 
 /* write callback for filling buffers with the response */
 static size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
 {
-	struct buffer *buffer = userdata;
+	rp_curl_buffer_t *buffer = userdata;
 	size_t sz = size * nmemb;
 	size_t old_size = buffer->size;
 	size_t new_size = old_size + sz;
-	char *data = realloc(buffer->data, new_size + 1);
-	if (!data)
-		return 0;
+	size_t needed = old_size + 1;
+	char *data = buffer->data;
+	if (needed > buffer->capacity) {
+		data = realloc(data, needed);
+		if (data == NULL)
+			return 0;
+		buffer->data = data;
+		buffer->capacity = needed;
+	}
 	memcpy(&data[old_size], ptr, sz);
 	data[new_size] = 0;
 	buffer->size = new_size;
-	buffer->data = data;
 	return sz;
 }
 
 /*
  * Perform the CURL operation for 'curl' and put the result in
- * memory. If 'result' isn't NULL it receives the returned content
- * that then must be freed. If 'size' isn't NULL, it receives the
- * size of the returned content. Note that if not NULL, the real
- * content is one byte greater than the read size and the last byte
- * zero. This facility allows to handle the returned content as a
- * null terminated C-string.
+ * memory managed by buffer.
+ * Note that the real content is one byte greater than the read
+ * size and the last byte zero. This facility allows to handle
+ * the returned content as a null terminated C-string.
  */
-int rp_curl_perform(CURL *curl, char **result, size_t *size)
-{
-	int rc;
-	struct buffer buffer;
-	CURLcode code;
-
-	/* init tthe buffer */
-	buffer.size = 0;
-	buffer.data = NULL;
-
-	/* Perform the request, res will get the return code */
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-
-	/* Perform the request, res will get the return code */
-	code = curl_easy_perform(curl);
-	rc = code == CURLE_OK;
-
-	/* Check for no errors */
-	if (rc) {
-		/* no error */
-		if (size)
-			*size = buffer.size;
-		if (result)
-			*result = buffer.data;
-		else
-			free(buffer.data);
-	} else {
-		/* had error */
-		if (size)
-			*size = 0;
-		if (result)
-			*result = NULL;
-		free(buffer.data);
-	}
-
-	return rc;
+int rp_curl_process(
+	CURL *curl,
+	struct curl_slist *headers,
+	rp_curl_buffer_t *buffer
+) {
+	buffer->size = 0;
+	return rp_curl_do(curl, headers, write_callback, buffer);
 }
 
-void rp_curl_do(CURL *curl, void (*callback)(void *closure, int status, CURL *curl, const char *result, size_t size), void *closure)
+/* cleanup of curl and headers */
+void rp_curl_cleanup(CURL *curl, struct curl_slist *headers)
 {
-	int rc;
-	char *result;
-	size_t size;
-	char errbuf[CURL_ERROR_SIZE];
-
-	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
-	rc = rp_curl_perform(curl, &result, &size);
-	if (rc)
-		callback(closure, rc, curl, result, size);
-	else
-		callback(closure, rc, curl, errbuf, 0);
-	free(result);
+	curl_slist_free_all(headers);
 	curl_easy_cleanup(curl);
 }
 
-int rp_curl_content_type_is(CURL *curl, const char *value)
+/* get a response header */
+const char *rp_curl_get_header(CURL * curl, const char *name)
 {
-	char *actual;
-	CURLcode code;
+	struct curl_header *hdr;
+	CURLHcode code;
 
-	code = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &actual);
-	if (code != CURLE_OK || !actual)
-		return 0;
-
-	return !strncasecmp(actual, value, strcspn(actual, "; "));
+	code = curl_easy_header(curl, name, 0, CURLH_HEADER, -1, &hdr);
+	return code == CURLHE_OK ? hdr->value : NULL;
 }
 
-long rp_curl_response_code_get(CURL *curl)
+/* get the response code */
+long rp_curl_get_response_code(CURL *curl)
 {
 	long rc;
 	CURLcode code;
@@ -185,7 +123,47 @@ long rp_curl_response_code_get(CURL *curl)
 	return (code == CURLE_OK) ? rc : 0;
 }
 
-CURL *rp_curl_prepare_get_url(const char *url)
+/* get the content type */
+const char *rp_curl_get_content_type(CURL *curl)
+{
+	return rp_curl_get_header(curl, content_type);
+}
+
+/* check the content type value */
+int rp_curl_content_type_is(CURL *curl, const char *value)
+{
+	const char *actual = rp_curl_get_content_type(curl);
+	return actual != NULL && !strncasecmp(actual, value, strcspn(actual, "; "));
+}
+
+int rp_curl_headers_add(struct curl_slist **headers, const char *value)
+{
+	struct curl_slist *newlist = curl_slist_append(*headers, value);
+	if (newlist == 0)
+		return -1;
+	*headers = newlist;
+	return 0;
+}
+
+int rp_curl_headers_add_keyval(struct curl_slist **headers, const char *name, const char *value)
+{
+	char *h;
+	int rc;
+
+	rc = asprintf(&h, "%s: %s", name, value);
+	if (rc >= 0) {
+		rc = rp_curl_headers_add(headers, h);
+		free(h);
+	}
+	return rc;
+}
+
+int rp_curl_headers_add_content_type(struct curl_slist **headers, const char *value)
+{
+	return rp_curl_headers_add_keyval(headers, content_type, value);
+}
+
+CURL *rp_curl_prepare_url(const char *url)
 {
 	CURL *curl;
 	CURLcode code;
@@ -200,100 +178,48 @@ CURL *rp_curl_prepare_get_url(const char *url)
 	return NULL;
 }
 
-CURL *rp_curl_prepare_get(const char *base, const char *path, const char * const *args)
+CURL *rp_curl_prepare(const char *base, const char *path, const char * const *args)
 {
 	CURL *res;
 	char *url;
 
 	url = rp_escape_url(base, path, args, NULL);
-	res = url ? rp_curl_prepare_get_url(url) : NULL;
+	if (url == NULL)
+		return NULL;
+
+	res = rp_curl_prepare_url(url);
 	free(url);
 	return res;
 }
 
-int rp_curl_add_header(CURL *curl, const char *header)
-{
-	int rc;
-	struct curl_slist *list;
+int rp_curl_post_func(
+	CURL *curl,
+	size_t (*callback)(char *buffer, size_t size, size_t nitems, void *userdata),
+	void *userdata
+) {
+	CURLcode code;
 
-	list = curl_slist_append(NULL, header);
-	rc = list ? curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list) == CURLE_OK : 0;
-/*
-	curl_slist_free_all(list);
-*/
-	return rc;
+	code = curl_easy_setopt(curl, CURLOPT_POST, 1L);
+	if (code == CURLE_OK)
+		code = curl_easy_setopt(curl, CURLOPT_READDATA, userdata);
+	if (code == CURLE_OK)
+		code = curl_easy_setopt(curl, CURLOPT_READFUNCTION, callback);
+	return -(code != CURLE_OK);
 }
 
-int rp_curl_add_header_value(CURL *curl, const char *name, const char *value)
+int rp_curl_post_data(CURL *curl, const char *data, size_t szdata, int copy)
 {
-	char *h;
-	int rc;
+	CURLcode code;
 
-	rc = asprintf(&h, "%s: %s", name, value);
-	rc = rc < 0 ? 0 : rp_curl_add_header(curl, h);
-	free(h);
-	return rc;
+	code = curl_easy_setopt(curl, CURLOPT_POST, 1L);
+	if (code == CURLE_OK)
+		code = curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, szdata);
+	if (code == CURLE_OK)
+		code = curl_easy_setopt(curl,
+				copy ? CURLOPT_COPYPOSTFIELDS : CURLOPT_POSTFIELDS,
+				data);
+	return -(code != CURLE_OK);
 }
 
 
-CURL *rp_curl_prepare_post_url_data(const char *url, const char *datatype, const char *data, size_t szdata)
-{
-	CURL *curl;
-
-	curl = curl_easy_init();
-	if (curl
-	&& CURLE_OK == curl_easy_setopt(curl, CURLOPT_URL, url)
-	&& (!szdata || CURLE_OK == curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, szdata))
-	&& CURLE_OK == curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data)
-	&& (!datatype || rp_curl_add_header_value(curl, "content-type", datatype)))
-		return curl;
-	curl_easy_cleanup(curl);
-	return NULL;
-}
-
-static CURL *rp_curl_prepare_post(const char *base, const char *path, int unescape_flag, const char *separator, const char * const *args, const char *simple_args)
-{
-	CURL *res;
-	char *url;
-	const char *data = NULL;
-	size_t szdata = 0;
-
-	url = rp_escape_url(base, path, NULL, NULL);
-	if(args) {
-		data = unescape_flag ?
-			curl_concatenate_args(args, separator, &szdata) :
-			rp_escape_args(args, &szdata);
-	}
-	else {
-		data = unescape_flag ?
-			rp_escape_str(simple_args, &szdata) :
-			simple_args;
-	}
-	szdata = szdata ? szdata : strlen(data);
-
-	res = url ? rp_curl_prepare_post_url_data(url, NULL, data, szdata) : NULL;
-	free(url);
-	return res;
-}
-
-CURL *rp_curl_prepare_post_simple_unescaped(const char *base, const char *path, const char *args)
-{
-	return rp_curl_prepare_post(base, path, 1, NULL, NULL, args);
-}
-
-CURL *rp_curl_prepare_post_simple_escaped(const char *base, const char *path, char *args)
-{
-	return rp_curl_prepare_post(base, path, 0, NULL, NULL, args);
-}
-
-CURL *rp_curl_prepare_post_unescaped(const char *base, const char *path, const char *separator, const char * const *args)
-{
-	return rp_curl_prepare_post(base, path, 1, separator, args, NULL);
-}
-
-CURL *rp_curl_prepare_post_escaped(const char *base, const char *path, const char * const *args)
-{
-	return rp_curl_prepare_post(base, path, 0, NULL, args, NULL);
-}
-
-/* vim: set colorcolumn=80: */
+/* vim: set cc=80: */
--- src/http/rp-curl.h
+++ src/http/rp-curl.h
@@ -20,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  */
 
 #pragma once
@@ -31,31 +30,186 @@
 extern "C" {
 #endif
 
-extern int rp_curl_perform (CURL * curl, char **result, size_t * size);
+/**
+ * Buffer used for storing read data
+ */
+typedef
+struct rp_curl_buffer {
+	/** size of the data read */
+	size_t size;
+	/** allocated size of the buffer */
+	size_t capacity;
+	/** pointer to allocated data */
+	char *data;
+}
+	rp_curl_buffer_t;
 
-extern void rp_curl_do(CURL *curl, void (*callback)(void *closure, int status, CURL *curl, const char *result, size_t size), void *closure);
+/**
+ * Perform the CURL query of 'curl' and put the result in
+ * the memory managed by 'buffer'.
+ * For convenience, a trailing zero is added after the received
+ * data but the size doesn't include it.
+ *
+ * The buffer must be valid. Using memset for setting all
+ * its field to zero is valid.
+ *
+ * The memory returned must be freed by free but it can also
+ * be reused in further queries.
+ *
+ * @param curl the CURL query to perform
+ * @param headers NULL or a list of headers to add
+ * @param buffer the buffer
+ *
+ * @result 0 on success or a negative code on error
+ */
+extern int rp_curl_process(
+		CURL * curl,
+		struct curl_slist *headers,
+		rp_curl_buffer_t *buffer);
 
+/**
+ * Perform the CURL query of 'curl' with given 'headers' list.
+ * the callback is called with download date and userdata.
+ *
+ * @param curl the CURL query to perform
+ * @param headers NULL or a list of headers to add
+ * @param callback callback to be called
+ * @param userdata closure of the callback
+ *
+ * @result 0 on success or a negative code on error
+ */
+extern int rp_curl_do(
+		CURL *curl,
+		struct curl_slist *headers,
+		size_t (*callback)(char *buffer, size_t size, size_t nitems, void *userdata),
+		void *userdata);
+
+/**
+ * Cleanup memory used.
+ *
+ * @param curl NULL or the CURL to cleanup
+ * @param headers NULL or the list to cleanup
+ */
+extern void rp_curl_cleanup(CURL *curl, struct curl_slist *headers);
+
+/**
+ * Extract the status of the reply of the query done by 'curl'.
+ *
+ * @param curl the CURL query object
+ *
+ * @return 0 on internal error or the response code (ex: 404)
+ */
+extern long rp_curl_get_response_code(CURL *curl);
+
+/**
+ * Get a header value (the first if repeated)
+ *
+ * @param curl the CURL query object
+ * @param name name of the header (without colon), case insensitive
+ *
+ * @return the value or NULL if error
+ */
+extern const char *rp_curl_get_header(CURL * curl, const char *name);
+
+/**
+ * Get the content type of the response.
+ *
+ * @param curl the CURL query object
+ *
+ * @return the content-type or NULL if error
+ */
+extern const char *rp_curl_get_content_type(CURL * curl);
+
+/**
+ * Check if the content_type of the reply of the query done by 'curl'
+ * is 'value'.
+ *
+ * @param curl the CURL query object
+ * @param value the expected mimy type
+ *
+ * @return 0 on internal error or the HTTP status code (ex: 404)
+ */
 extern int rp_curl_content_type_is (CURL * curl, const char *value);
 
-extern long rp_curl_response_code_get(CURL *curl);
+/**
+ * Prepares a simple CURL query (defaulting to type GET)
+ *
+ * @param url the URL to be queried
+ *
+ * @return the simple CURL handler for the query
+ */
+extern CURL *rp_curl_prepare_url(const char *url);
 
-extern CURL *rp_curl_prepare_get_url(const char *url);
+/**
+ * Prepares a simple CURL query of type GET (defaulting to type GET)
+ *
+ * @param base base name of the URL (ex: "https://u:p@server.int:4444/appli")
+ * @param path NULL or path within base (ex: "api/v1/get")
+ * @param args NULL or a NULL terminated array of pairs key (even) value (odd, can be NULL)
+ *
+ * @return the simple CURL handler for the query
+ *
+ * @note @see rp_escape_url
+ */
+extern CURL *rp_curl_prepare(const char *base, const char *path, const char * const *args);
 
-extern CURL *rp_curl_prepare_get(const char *base, const char *path, const char * const *args);
+/**
+ * Add a header to the list of headers
+ *
+ * @param headers the list of headers (NULL if empty)
+ * @param value the "key: value" string
+ *
+ * @return 0 on success or a negative code on error
+ */
+extern int rp_curl_headers_add(struct curl_slist **headers, const char *value);
 
-extern CURL *rp_curl_prepare_post_url_data(const char *url, const char *datatype, const char *data, size_t szdata);
+/**
+ * Add a header "key: value" to the list of headers
+ *
+ * @param headers the list of headers (NULL if empty)
+ * @param key the key string
+ * @param value the value string
+ *
+ * @return 0 on success or a negative code on error
+ */
+extern int rp_curl_headers_add_keyval(struct curl_slist **headers, const char *name, const char *value);
 
-extern int rp_curl_add_header(CURL *curl, const char *header);
+/**
+ * Shortcup to rp_curl_headers_add_keyval(headers, "content-type" value)
+ *
+ * @param headers the list of headers (NULL if empty)
+ * @param value the content type to add
+ *
+ * @return 0 on success or a negative code on error
+ */
+extern int rp_curl_headers_add_content_type(struct curl_slist **headers, const char *value);
 
-extern int rp_curl_add_header_value(CURL *curl, const char *name, const char *value);
+/**
+ * Make a post request with the given 'callback' and 'userdata'.
+ *
+ * @param curl the CURL object
+ * @param callback the callback function for getting data
+ * @param userdata to pass to the callback
+ *
+ * @return 0 on success or NULL on error
+ */
+extern int rp_curl_post_func(
+		CURL *curl,
+		size_t (*callback)(char *buffer, size_t size, size_t nitems, void *userdata),
+		void *userdata);
 
-extern CURL *rp_curl_prepare_post_simple_unescaped(const char *base, const char *path, const char *args);
-
-extern CURL *rp_curl_prepare_post_simple_escaped(const char *base, const char *path, char *args);
-
-extern CURL *rp_curl_prepare_post_unescaped(const char *base, const char *path, const char *separator, const char * const *args);
-
-extern CURL *rp_curl_prepare_post_escaped(const char *base, const char *path, const char * const *args);
+/**
+ * Make a post request with the given 'data' of given 'size'.
+ * Data are copied if 'copy' isn't nul.
+ *
+ * @param curl the CURL object
+ * @param data the data to pass
+ * @param size the size of the data
+ * @param copy a flag telling to copy the data if not null
+ *
+ * @return 0 on success or NULL on error
+ */
+extern int rp_curl_post_data(CURL *curl, const char *data, size_t szdata, int copy);
 
 #ifdef	__cplusplus
 }
